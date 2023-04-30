f3c7f1a9d0d0f5624da3a0624043e99e8ea8727e
mandelshtamd <Ofwgktadgaf1984>
2023-04-30 14:59:28 +0300

    add 6 homework

---
+++ AdoNet lab/AdoNet lab/PlayerRepository.cs
@@ -0,0 +1,153 @@
+﻿using System.Data.SqlClient;
+using AdoNet_lab.Model;
+
+namespace AdoNet_Lab
+{
+    public static class PlayerRepository
+    {
+        public static void CreatePlayersTable(SqlConnection connection)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandText =
+                "CREATE TABLE Player (ID INT PRIMARY KEY IDENTITY, FirstName NVARCHAR(100) NOT NULL, " +
+                "SecondName NVARCHAR(100) NOT NULL, AddressID INT, TeamID INT, NumberOfGoals INT) ";
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+        }
+
+        public static void CreateAddressTable(SqlConnection connection)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandText =
+                "CREATE TABLE Address (ID INT PRIMARY KEY IDENTITY, Country NVARCHAR(100) NOT NULL, " +
+                "City NVARCHAR(100) NOT NULL, Street NVARCHAR(100) NOT NULL, Building INT, Appartment INT) ";
+            command.Connection = connection;
+            command.ExecuteNonQuery();
+        }
+
+        public static void AddPlayer(SqlConnection connection, string firstName, string secondName, int teamID,
+            Address address)
+        {
+            SqlTransaction transaction = connection.BeginTransaction();
+            SqlCommand command = connection.CreateCommand();
+            command.Transaction = transaction;
+
+            try
+            {
+                command.CommandText = string.Format(
+                    "INSERT INTO Address (Country, City, Street, Building, Appartment)" +
+                    " VALUES ('{0}', '{1}', '{2}', {3}, {4})", address.Country, address.City, address.Street,
+                    address.Building, address.Appartment);
+                command.ExecuteNonQuery();
+
+                command.CommandText = "SELECT TOP 1 (ID) FROM Address ORDER BY ID DESC";
+                var result = command.ExecuteScalar().ToString();
+                var addressID = int.Parse(result);
+                command.CommandText = "INSERT INTO Player (FirstName, SecondName, AddressID, TeamID, NumberOfGoals)" +
+                                      $" VALUES ('{firstName}', '{secondName}', {addressID}, {teamID}, 0)";
+                command.ExecuteNonQuery();
+
+                command.CommandText =
+                    $"UPDATE Stats SET NumberOfPlayers=NumberOfPlayers+1 WHERE TeamID={teamID}";
+                command.ExecuteNonQuery();
+
+                transaction.Commit();
+                Console.WriteLine("Player added successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                transaction.Rollback();
+            }
+        }
+
+        public static void ScoreGoal(SqlConnection connection, int playerID)
+        {
+            SqlTransaction transaction = connection.BeginTransaction();
+            SqlCommand command = connection.CreateCommand();
+            command.Transaction = transaction;
+
+            try
+            {
+                command.CommandText = $"UPDATE Player SET NumberOfGoals=NumberOfGoals+1 WHERE ID={playerID}";
+                command.ExecuteNonQuery();
+                
+                command.CommandText = $"SELECT TeamID FROM Player WHERE ID={playerID}";
+                var result = command.ExecuteScalar().ToString();
+                var teamID = int.Parse(result);
+                command.CommandText = $"SELECT ROUND(AVG(NumberOfGoals), 1) FROM Player WHERE TeamID={teamID}";
+                int avgGoals = int.Parse(command.ExecuteScalar().ToString());
+                command.CommandText = $"UPDATE Stats SET AverageGoalsPerPlayer={avgGoals} WHERE TeamID={teamID}";
+                command.ExecuteNonQuery();
+
+                transaction.Commit();
+                Console.WriteLine("Goal score added successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                transaction.Rollback();
+            }
+        }
+
+        public static void ChangeTeam(SqlConnection connection, int playerID, int newTeamID)
+        {
+            SqlTransaction transaction = connection.BeginTransaction();
+            SqlCommand command = connection.CreateCommand();
+            command.Transaction = transaction;
+
+            try
+            {
+                command.CommandText = $"SELECT TeamID FROM Player WHERE ID={playerID}";
+                int oldTeamID = int.Parse((command.ExecuteScalar().ToString()));
+                command.CommandText = $"UPDATE Player SET TeamID={newTeamID} WHERE ID={playerID}";
+                command.ExecuteNonQuery();
+                
+                command.CommandText =
+                    $"UPDATE Stats SET NumberOfPlyers=NumberOfPlayers-1 WHERE TeamID={oldTeamID}";
+                command.ExecuteNonQuery();
+                
+                command.CommandText =
+                    $"UPDATE Stats SET NumberOfPlyers=NumberOfPlayers+1 WHERE TeamID={newTeamID}";
+                command.ExecuteNonQuery();
+
+                transaction.Commit();
+                Console.WriteLine("Team changed successfully");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                transaction.Rollback();
+            }
+        }
+
+        public static void DeletePlayer(SqlConnection connection, int playerID)
+        {
+            SqlTransaction transaction = connection.BeginTransaction();
+            SqlCommand command = connection.CreateCommand();
+            command.Transaction = transaction;
+
+            try
+            {
+                command.CommandText = $"SELECT TeamID FROM Player WHERE ID={playerID}";
+                var result = command.ExecuteScalar().ToString();
+                var teamID = int.Parse(result);
+                command.CommandText = $"DELETE FROM Player WHERE ID={playerID}";
+                command.ExecuteNonQuery();
+                
+                command.CommandText =
+                    $"UPDATE Stats SET NumberOfPlyers=NumberOfPlayers-1 WHERE TeamID={teamID}";
+                command.ExecuteNonQuery();
+
+                transaction.Commit();
+                Console.WriteLine("Player deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                transaction.Rollback();
+            }
+        }
+    }
+}
---
+++ AdoNet lab/AdoNet lab/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using AdoNet_Lab;
+using AdoNet_lab.Model;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            // PlayerRepository.CreatePlayersTable(connection);
+            // Console.WriteLine("Таблица Players создана");
+            //
+            // PlayerRepository.CreateAddressTable(connection);
+            // Console.WriteLine("Таблица Address создана");
+            //
+            // TeamRepository.CreateStatsTable(connection);
+            // Console.WriteLine("Таблица Stats создана");
+            //
+            // TeamRepository.CreateTeamTable(connection);
+            // Console.WriteLine("Таблица Team создана");
+            //
+            // StadiumRepository.CreateStadiumTable(connection);
+            // Console.WriteLine("Таблица Stadium создана");
+            //
+            // TeamRepository.CreateTeamStadiumTable(connection);
+            // Console.WriteLine("Таблица TeamStadium создана");
+            
+            
+            var address = new Address("Russia", "Moscow", "Tolstova str.", 16, 15);
+            TeamRepository.AddTeam(connection, "Zenit", 1925);
+            StadiumRepository.AddStadium(connection, "Zenit Arena");
+            TeamRepository.AddStadium(connection, 1, 1);
+            PlayerRepository.AddPlayer(connection, "Anton", "Antonov", 1, address);
+            TeamRepository.GetTeamStats(connection, 1);
+        }
+        Console.WriteLine("Подключение закрыто...");
+        Console.Read();
+    }
+}
